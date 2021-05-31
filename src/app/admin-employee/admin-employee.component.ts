871df20fcfcf4b41e374744f1976b37304a10b00
mashg <34698866+mashg@users.noreply.github.com>
2021-06-01 02:54:20 +0300

    admin component search button1

--- src/app/admin-employee/admin-employee.component.ts
+++ src/app/admin-employee/admin-employee.component.ts
@@ -5,6 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin-employee.component.html',
   styleUrls: ['./admin-employee.component.css']
 })
+
+@Component({
+  selector: 'form-field-overview-example',
+  templateUrl: 'form-field-overview-example.html',
+  styleUrls: ['form-field-overview-example.css']
+})
+export class FormFieldOverviewExample {}
 export class AdminEmployeeComponent implements OnInit {
 
   constructor() { }
