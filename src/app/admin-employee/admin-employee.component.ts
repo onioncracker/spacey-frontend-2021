153eb2fdf918c9c895bc17d87af7aa83c9eeebe0
mashg <34698866+mashg@users.noreply.github.com>
2021-06-01 01:52:48 +0300

    admin component init

---
+++ src/app/admin-employee/admin-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-employee',
+  templateUrl: './admin-employee.component.html',
+  styleUrls: ['./admin-employee.component.css']
+})
+export class AdminEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
