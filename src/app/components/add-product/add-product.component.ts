c2e08b8c5d7d72e90e13b8e39ce6ac9d2111ef49
Katatyunyaa <katerynahaletska@gmail.com>
2021-06-09 17:10:41 +0300

    fixed duplication

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -3,18 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { AddProductService } from '../../store/service/add-product/add-product.service';
 import { FormControl } from '@angular/forms';
 
-interface Category {
-  value1: string;
-  viewValue1: string;
-}
-interface Sex {
-  value2: string;
-  viewValue2: string;
-}
-interface Color {
-  value3: string;
-  viewValue3: string;
-}
+interface Category {}
+interface Sex {}
+interface Color {}
 
 @Component({
   selector: 'app-add-product',
@@ -23,33 +14,13 @@ interface Color {
 })
 export class AddProductComponent {
   toppings = new FormControl();
-  toppingList: string[] = [
-    'Cotton',
-    'Synthetics',
-    'Viscose',
-    'Silk',
-    'Wool',
-    'Polyester',
-  ];
+  toppingList: string[] = [];
 
   selectedValue!: string;
 
-  categories: Category[] = [
-    { value1: 'T-shorts-0', viewValue1: 'T-shorts' },
-    { value1: 'Jeans-1', viewValue1: 'Jeans' },
-    { value1: 'Shorts-2', viewValue1: 'Shorts' },
-  ];
-
-  sexes: Sex[] = [
-    { value2: 'MEN', viewValue2: 'MEN' },
-    { value2: 'WOMEN', viewValue2: 'WOMEN' },
-  ];
-
-  colors: Color[] = [
-    { value3: 'Red', viewValue3: 'Red' },
-    { value3: 'Pink', viewValue3: 'Pink' },
-    { value3: 'Brown', viewValue3: 'Brown' },
-  ];
+  categories: Category[] = [];
+  sexes: Sex[] = [];
+  colors: Color[] = [];
 
   constructor(
     private route: ActivatedRoute,
