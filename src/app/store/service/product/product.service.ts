08fbe09182400e7c1f1f53c9366e4cf2e0399d3c
mashg <34698866+mashg@users.noreply.github.com>
2021-06-15 13:45:01 +0300

    Admin Manage + Admin Adding + Admin Edit

--- src/app/store/service/product/product.service.ts
+++ src/app/store/service/product/product.service.ts
@@ -7,8 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ProductService {
-  private productUrl = `${environment.url}/api/v1/products`;
-
+  private productUrl = `${environment.url}/products`;
 
   constructor(private http: HttpClient) {}
 
