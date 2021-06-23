04f869fe4baa52723a90554aab34203260d7d4bb
arkile <37909996+arkile@users.noreply.github.com>
2021-06-23 23:50:00 +0300

    Update AuthService.ts

--- src/app/store/service/auth/AuthService.ts
+++ src/app/store/service/auth/AuthService.ts
@@ -15,7 +15,7 @@ export class AuthService {
   private registerURL = environment.url + endpointUrls.apiPrefix + '/register';
   private loginURL = environment.url + endpointUrls.apiPrefix + '/login';
   private confirmURL =
-    environment.url + endpointUrls.apiPrefix + '/registration-confirm';
+    environment.url + endpointUrls.apiPrefix + '/confirm-registration';
 
   private loggedIn = false;
   private httpOptions = { observe: 'response' as const };
